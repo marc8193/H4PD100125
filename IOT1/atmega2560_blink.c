61e1658b659b4ba9a259b9d0b69ee300ed090d31
Marcus Haukelid Larsen <mhl@rtx.dk>
2025-11-10 13:57:26 +0100

    Added internal blink

---
+++ IOT1/atmega2560_blink.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED_PIN PB7
+
+int main(void)
+{
+    DDRB = 0xFF;
+
+    while (1) {
+        PORTB ^= (1 << LED_PIN);
+        _delay_ms(500);
+    }
+}
