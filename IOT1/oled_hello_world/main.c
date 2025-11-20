59ec689ea6076f14929619d85d991e3a495f202c
Marcus Haukelid Larsen <mhl@rtx.dk>
2025-11-20 11:42:18 +0100

    Fixed global interrupt init

--- IOT1/oled_hello_world/main.c
+++ IOT1/oled_hello_world/main.c
@@ -19,24 +19,22 @@ int main(void) {
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);  // Enable RX + + TX + RX interrupt
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8-bit data
 
-    sei();	// Enable global interrupts
-
 	stdout = &uart_output;
 
 	uint8_t display_status = SSD1306_Init(SSD1306_ADDR);
 	if (SSD1306_SUCCESS != display_status) {
 		printf("Display NOT properly initialized!\r\n");
-	} else {
-		printf("Display has been initialized.\r\n");
 	}
 
-    while (1) {
-		display_status = SSD1306_DrawString("h4pd011125 styrer", NORMAL);
-		if (SSD1306_SUCCESS != display_status) {
-			printf("Error while trying to draw string to screen - %d\r\n", 
-					display_status);
-		}
+    sei();	// Enable global interrupts
+
+	display_status = SSD1306_DrawString("Hello, world.", NORMAL);
+	if (SSD1306_SUCCESS != display_status) {
+		printf("Error while trying to draw string to screen - %d\r\n", 
+				display_status);
 	}
+
+    while (1) {}
 
     return 0;
 }
